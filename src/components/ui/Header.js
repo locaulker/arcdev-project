276e5d6a9394955d3897f6246b780e462ab64575
Lawrence <locaulker@gmail.com>
2022-12-14 01:37:48 -0500

    added button styles

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,9 @@ import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import useScrollTrigger from "@material-ui/core/useScrollTrigger"
 import { makeStyles } from "@material-ui/styles"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
+import Button from "@material-ui/core/Button"
 
 import logo from "../../assets/logo.svg"
 
@@ -11,37 +14,62 @@ function ElevationScroll(props) {
 
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 0,
+    threshold: 0
   })
 
   return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
+    elevation: trigger ? 4 : 0
   })
 }
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em",
+    marginBottom: "3em"
   },
   logo: {
-    height: "7em",
+    height: "7em"
   },
+  tabContainer: {
+    marginLeft: "auto"
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px"
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+    padding: "10px 25px"
+  }
 }))
 
 export default function Header(props) {
   const classes = useStyles()
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed">
-<<<<<<< HEAD
-          <Toolbar>
-            <img src={logo} alt="company logo" />
-=======
           <Toolbar disableGutters={true}>
             <img className={classes.logo} src={logo} alt="Company Logo" />
->>>>>>> master
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
