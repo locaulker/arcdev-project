04d2318abd7b0c0ae348ad6609db702c562c3e85
Lawrence <locaulker@gmail.com>
2022-12-15 01:51:21 -0500

    resume at L29

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import useScrollTrigger from "@material-ui/core/useScrollTrigger"
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/styles"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
 import Button from "@material-ui/core/Button"
+import { Link } from "react-router-dom"
 
 import logo from "../../assets/logo.svg"
 
@@ -14,41 +15,46 @@ function ElevationScroll(props) {
 
   const trigger = useScrollTrigger({
     disableHysteresis: true,
-    threshold: 0,
+    threshold: 0
   })
 
   return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
+    elevation: trigger ? 4 : 0
   })
 }
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em",
+    marginBottom: "3em"
   },
   logo: {
-    height: "7em",
+    height: "7em"
   },
   tabContainer: {
-    marginLeft: "auto",
+    marginLeft: "auto"
   },
   tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "25px",
+    marginLeft: "25px"
   },
   button: {
     ...theme.typography.estimate,
     borderRadius: "50px",
     marginLeft: "50px",
     marginRight: "25px",
-    height: "45px",
-  },
+    height: "45px"
+  }
 }))
 
 export default function Header(props) {
   const classes = useStyles()
+  const [value, setValue] = useState(0)
+
+  const handleChange = (e, value) => {
+    setValue(value)
+  }
 
   return (
     <React.Fragment>
@@ -56,12 +62,42 @@ export default function Header(props) {
         <AppBar position="fixed">
           <Toolbar disableGutters={true}>
             <img className={classes.logo} src={logo} alt="Company Logo" />
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="secondary"
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
             </Tabs>
             <Button
               variant="contained"
