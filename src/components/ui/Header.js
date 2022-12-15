0e9e27e140a2f933d8682dfbe940fd34443977b8
Lawrence <locaulker@gmail.com>
2022-12-15 06:12:45 -0500

    resume L29 @9:50

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import useScrollTrigger from "@material-ui/core/useScrollTrigger"
@@ -29,7 +29,10 @@ const useStyles = makeStyles(theme => ({
     marginBottom: "3em"
   },
   logo: {
-    height: "7em"
+    height: "8em"
+  },
+  logoContainer: {
+    padding: 0
   },
   tabContainer: {
     marginLeft: "auto"
@@ -56,17 +59,40 @@ export default function Header(props) {
     setValue(value)
   }
 
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0)
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1)
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2)
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3)
+    } else if (window.location.pathname === "/contsct" && value !== 4) {
+      setValue(4)
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5)
+    }
+  }, [value])
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters={true}>
-            <img className={classes.logo} src={logo} alt="Company Logo" />
+            <Button
+              component={Link}
+              to="/"
+              onClick={() => setValue(0)}
+              className={classes.logoContainer}
+            >
+              <img className={classes.logo} src={logo} alt="Company Logo" />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChange}
               className={classes.tabContainer}
-              indicatorColor="secondary"
+              indicatorColor="primary"
             >
               <Tab
                 className={classes.tab}
